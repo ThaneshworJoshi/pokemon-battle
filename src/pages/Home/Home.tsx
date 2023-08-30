66e1e971035a5732bc1f01d1cf29a0eef55baedf
Thaneshwor Joshi <thaneshwor10@gmail.com>
2023-08-30 22:12:24 +0545

    feat: Implement styled home component

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,33 @@
 import React from "react";
+import { Herobanner } from "components";
+import { useNavigate } from "react-router-dom";
+import "./Home.scss";
 
 export const Home = () => {
-  return <div>Home</div>;
+  const navigate = useNavigate();
+
+  const heroBannerData = {
+    caption: "Welcom to",
+    title: "Battle Pokemon",
+    description:
+      "Battle Pokemon is  a fun and enagaging way to battle between pokemons, whether for casual play or move serious competition between the pokemons.",
+    ctaButton: {
+      title: "Start Battle",
+      onClick: () => {
+        navigate("/battle");
+      },
+    },
+    media: {
+      imageUrl: "./assets/banner.png",
+      altText: "Hero Image",
+    },
+  };
+
+  return (
+    <div className="home-container">
+      <Herobanner {...heroBannerData} />
+    </div>
+  );
 };
 
 export default Home;
