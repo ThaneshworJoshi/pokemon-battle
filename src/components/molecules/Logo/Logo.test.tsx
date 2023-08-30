fd4fcd2232f5c2ec1590a2806ae1e93efaa582f7
Thaneshwor Joshi <thaneshwor10@gmail.com>
2023-08-30 22:10:11 +0545

    fix: test fail issue

--- src/components/molecules/Logo/Logo.test.tsx
+++ src/components/molecules/Logo/Logo.test.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { fireEvent, render } from "@testing-library/react";
 import { Logo } from "./Logo.component";
+import { MemoryRouter } from "react-router-dom";
 
 describe("Logo component", () => {
   const logoData = {
@@ -12,16 +13,28 @@ describe("Logo component", () => {
   };
 
   it("renders without crashing", () => {
-    render(<Logo {...logoData} />);
+    render(
+      <MemoryRouter>
+        <Logo {...logoData} />{" "}
+      </MemoryRouter>
+    );
   });
 
   it("renders with default props", () => {
-    const { getByAltText } = render(<Logo {...logoData} />);
+    const { getByAltText } = render(
+      <MemoryRouter>
+        <Logo {...logoData} />{" "}
+      </MemoryRouter>
+    );
     expect(getByAltText("logo alt")).toBeInTheDocument();
   });
 
   it("calls onClick event", () => {
-    const { getByRole } = render(<Logo {...logoData} />);
+    const { getByRole } = render(
+      <MemoryRouter>
+        <Logo {...logoData} />{" "}
+      </MemoryRouter>
+    );
     const logoLink = getByRole("link");
     expect(logoLink).toHaveAttribute("href", logoData.link);
   });
