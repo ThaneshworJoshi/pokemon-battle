14c26c084a43d1222d691fb12d84b96b4379ab6f
Thaneshwor Joshi <thaneshwor10@gmail.com>
2023-08-30 21:48:58 +0545

    feat: Create initial layout with background and header

--- src/common/components/layout/AppLayout.tsx
+++ src/common/components/layout/AppLayout.tsx
@@ -1,12 +1,31 @@
 import React from "react";
+import { Header } from "components/organisms/Header";
 import { Outlet } from "react-router-dom";
+import "./AppLayout.scss";
+
+const headerData = {
+  navData: {
+    itemList: [
+      { label: "Home", link: "/" },
+      { label: "Battle", link: "/battle" },
+      { label: "History", link: "/history" },
+      { label: "Pokedex", link: "/pokedex" },
+      { label: "About", link: "/about" },
+    ],
+  },
+  logoData: {
+    media: {
+      imageUrl: "./assets/logo.png",
+      altText: "logo alt",
+    },
+    link: "/",
+  },
+};
 
 export const AppLayout = () => {
   return (
-    <div>
-      <div>
-        <header>Header</header>
-      </div>
+    <div className="layout">
+      <Header {...headerData} />
       <div id="content">
         <Outlet />
       </div>
--- src/components/molecules/Logo/Logo.component.tsx
+++ src/components/molecules/Logo/Logo.component.tsx
@@ -1,17 +1,18 @@
 import React, { FC } from "react";
 import { LogoProps } from "./Logo.type";
+import { Link } from "react-router-dom";
 import "./Logo.scss";
 
 export const Logo: FC<LogoProps> = ({ media, link }) => {
   return (
     <div className="logo">
-      <a href={link || "/"} className="logo-link">
+      <Link to={link || "/"} className="logo-link">
         <img
           src={media.imageUrl}
           alt={media?.altText || "Logo"}
           className="logo-image"
         />
-      </a>
+      </Link>
     </div>
   );
 };
