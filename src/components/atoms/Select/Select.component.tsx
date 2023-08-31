0825b9aa4c5ec38de2b5f27c607b581c7092f484
Thaneshwor Joshi <thaneshwor10@gmail.com>
2023-08-31 20:58:30 +0545

    feat: Create Battle page

--- src/components/atoms/Select/Select.component.tsx
+++ src/components/atoms/Select/Select.component.tsx
@@ -1,37 +1,26 @@
-// import React, { FC } from "react";
-// import { SelectProps } from "./Select.type";
-// import "./Select.scss";
-
-// export const Select: FC<SelectProps> = () => {
-//   return <div>Select</div>;
-// };
-
-// export default Select;
-
-// PokemonSearch.js
-import React, { FC, useState } from "react";
-import "./Select.scss";
-import { SelectProps } from "./Select.type";
+import React, { FC, useState } from 'react'
+import './Select.scss'
+import { SelectProps } from './Select.type'
 
 export const Select: FC<SelectProps> = ({ fullWidth, styles, items }) => {
-  const [filter, setFilter] = useState("");
-  const [showList, setShowList] = useState(true);
+  const [filter, setFilter] = useState('')
+  const [showList, setShowList] = useState(false)
   const filteredItem = items.filter((pokemon) =>
     pokemon.toLowerCase().includes(filter.toLowerCase())
-  );
+  )
 
   const handleItemClick = (item: string) => {
-    setFilter(item);
-    setShowList(false);
-  };
+    setFilter(item)
+    setShowList(false)
+  }
 
   const onChangeHandler = (event: any) => {
-    setFilter(event.target.value);
-    setShowList(true);
-  };
+    setFilter(event.target.value)
+    setShowList(true)
+  }
 
   return (
-    <div className={`search ${fullWidth ? "full-width" : ""}`} {...styles}>
+    <div className={`search ${fullWidth ? 'full-width' : ''}`} {...styles}>
       <div className="search__input-container">
         <button className="search__button--left" disabled>
           <img src="./assets/ball.png" className="search__image--left" />
@@ -60,7 +49,7 @@ export const Select: FC<SelectProps> = ({ fullWidth, styles, items }) => {
         </ul>
       )}
     </div>
-  );
-};
+  )
+}
 
-export default Select;
+export default Select
--- src/pages/Battle/Battle.tsx
+++ src/pages/Battle/Battle.tsx
@@ -1,7 +1,68 @@
-import React from "react";
+import React from 'react'
+import { BattleCard } from 'components/molecules'
+import './Battle.scss'
+import { Button, Select } from 'components/atoms'
 
+const pokemons = [
+  'Bulbasaur',
+  'Charmander',
+  'Squirtle',
+  'Pikachu',
+  'Eevee',
+  'Jigglypuff',
+  'Snorlax',
+  'Mewtwo',
+  'Gengar',
+  'Dragonite',
+]
+
+const mockPokemon1 = {
+  name: 'Blastoise',
+  type: 'Water',
+  hp: 267,
+  moves: ['Flash Cannon', 'Hyper Beam', 'Aqua Ring', 'Hydro Canon'],
+  media: {
+    imageUrl: './assets/pok1.png',
+    altText: 'alt-text',
+  },
+}
+
+const mockPokemon2 = {
+  name: 'Bulbasour',
+  type: 'Grass',
+  hp: 345,
+  moves: ['Razor Leaf', 'Seed Bomb', 'Solar Beam', 'Rock Smash'],
+  media: {
+    imageUrl: './assets/pok2.png',
+    altText: 'alt-text',
+  },
+}
 export const Battle = () => {
-  return <div>Battle</div>;
-};
+  return (
+    <div className="container">
+      <div className="battle">
+        <h3 className="battle__title">Lets the Battle Begin</h3>
 
-export default Battle;
+        <div className="battle__box">
+          <div className="battle__card-wrapper">
+            <Select items={pokemons} fullWidth={false} />
+            <BattleCard {...mockPokemon1} />
+          </div>
+          <div className="battle__vs-img">
+            <img src="./assets/vs.png" alt="vs alt" />
+          </div>
+          <div className="battle__card-wrapper">
+            <Select items={pokemons} fullWidth={false} />
+
+            <BattleCard {...mockPokemon2} />
+          </div>
+        </div>
+        <div className="battle__cta">
+          <Button size="big">Start Battle</Button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Battle
