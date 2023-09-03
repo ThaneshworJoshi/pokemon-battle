fde2719857a03e550d5f3fcfba0018b4120f7f76
Thaneshwor Joshi <thaneshwor10@gmail.com>
2023-09-03 12:38:27 +0545

    feat: Implement debounce for API calls in Battle component

--- src/pages/Battle/Battle.tsx
+++ src/pages/Battle/Battle.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useMemo, useState } from 'react'
 import { BattleCard, BattleHistoryItemProps } from 'components/molecules'
 import './Battle.scss'
 import { Button, Select } from 'components/atoms'
@@ -17,13 +17,14 @@ import { simulateBattle } from 'utils/simulateBattle'
 import { getRandomId } from 'utils/helper'
 import { getCurrentDate, getCurrentTime } from 'utils/datetime'
 import { saveDataToLocalStorage } from 'utils/localStorageUtils'
+import { debounce } from 'utils/debounce'
 
 export const Battle = () => {
   const dispatch = useAppDispatch()
   const [pokemons, setPokemons] = useState([])
   const [leftPokemon, setLeftPokemon] = useState('')
-  const [startBattle, setStartBattle] = useState(false)
   const [rightPokemon, setRightPokemon] = useState('')
+  const [startBattle, setStartBattle] = useState(false)
   const [currentPokemonInfo, setCurrentPokemonInfo] = useState({
     name: '',
     position: '',
@@ -41,17 +42,33 @@ export const Battle = () => {
     (state) => state?.pokemon
   )
 
+  // Function to update currentPokemonInfo based on the type (left/right) and pokemon name
+  const updatePokemonInfo = useCallback((type: string, name: string) => {
+    if (type === 'left') {
+      setCurrentPokemonInfo({ name: name, position: 'left' })
+    } else {
+      setCurrentPokemonInfo({ name: name, position: 'right' })
+    }
+  }, [])
+
+  // Create a debounced version of the updatePokemonInfo function
+  const debouncedSearch = useMemo(() => {
+    return debounce(updatePokemonInfo, 500)
+  }, [currentPokemonInfo])
+
+  // Handler for left Pokemon change
   const onLeftPokemonChange = (pokemon: string) => {
     setLeftPokemon(pokemon)
   }
 
+  // Handler for right Pokemon change
   const onRightPokemonChange = (pokemon: string) => {
     setRightPokemon(pokemon)
   }
 
+  // Handler for starting or resetting the battle
   const handleBattleStart = () => {
     if (startBattle) {
-      // setBattleResult({})
       setStartBattle(false)
       setLeftPokemon('')
       setRightPokemon('')
@@ -77,17 +94,21 @@ export const Battle = () => {
         { name: loser.name, imageUrl: loser?.media?.imageUrl },
       ],
     }
+    // Save battle info to local storage
     saveDataToLocalStorage(battleInfo)
+    // Dispatch the winner and battle history
     dispatch(updateWinner({ name: winner?.name, isWinner: true }))
     dispatch(addBattleHistory(battleInfo))
   }
 
+  // Clear opponents from Redux store when unmounting the component
   useEffect(() => {
     return () => {
       dispatch(clearOpponents())
     }
   }, [])
 
+  // Update the list of available pokemons and selected opponent based on API data and selected position
   useEffect(() => {
     if (status === 'success') {
       const pokemons = data?.results?.map((pokemon: any) => pokemon.name)
@@ -102,12 +123,14 @@ export const Battle = () => {
     }
   }, [status, pokemonStatus])
 
+  // Update currentPokemonInfo for left Pokemon with debounce
   useEffect(() => {
-    setCurrentPokemonInfo({ name: leftPokemon, position: 'left' })
+    debouncedSearch('left', leftPokemon)
   }, [leftPokemon])
 
+  // Update currentPokemonInfo for right Pokemon with debounce
   useEffect(() => {
-    setCurrentPokemonInfo({ name: rightPokemon, position: 'right' })
+    debouncedSearch('right', rightPokemon)
   }, [rightPokemon])
 
   return (
