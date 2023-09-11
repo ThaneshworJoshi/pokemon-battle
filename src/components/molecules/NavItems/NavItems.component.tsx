1d480833a83bf81f8b4b37bec1388f58a837dc49
Thaneshwor Joshi <thanos@soanitech.com>
2023-09-11 19:09:27 +0545

    feat: Implement Responsive Design for Header Menu Item

--- src/components/molecules/NavItems/NavItems.component.tsx
+++ src/components/molecules/NavItems/NavItems.component.tsx
@@ -1,24 +1,27 @@
-import React, { type FC } from 'react'
+import React, { useState, type FC } from 'react'
 import { type NavItemsProps } from './NavItems.type'
 import './NavItems.scss'
 import { NavLink, useLocation } from 'react-router-dom'
 
 export const NavItems: FC<NavItemsProps> = ({ itemList }) => {
   const { pathname } = useLocation()
+  const [visible, setVisible] = useState(true)
+
+  const toggleSidebar = () => {
+    setVisible(!visible)
+  }
 
   return (
     <nav className="nav">
-      <ul className="nav__list">
+      <ul className={`nav__list ${visible ? 'visible' : 'hidden'}`}>
         {itemList.map((item, index) => (
           <li key={index} className="nav__item">
             <NavLink to={item?.link} className="nav__item-link">
-              {pathname === item?.link && (
-                <img
-                  src="./assets/ball.png"
-                  alt="active-image-ball"
-                  className="nav__item-image"
-                />
-              )}
+              <span className="nav__item-image">
+                {pathname === item?.link && (
+                  <img src="./assets/ball.png" alt="active-image-ball" />
+                )}
+              </span>
               <span
                 className={`nav__item-label ${
                   pathname === item?.link ? 'active-item' : ''
@@ -29,7 +32,16 @@ export const NavItems: FC<NavItemsProps> = ({ itemList }) => {
             </NavLink>
           </li>
         ))}
+        <button
+          className="nav__toggle-btn nav__close-btn"
+          onClick={toggleSidebar}
+        >
+          <img src="./assets/cross.png" className="nav__toggle-img" />
+        </button>
       </ul>
+      <button className="nav__toggle-btn" onClick={toggleSidebar}>
+        <img src="./assets/menu.png" className="nav__toggle-img" />
+      </button>
     </nav>
   )
 }
