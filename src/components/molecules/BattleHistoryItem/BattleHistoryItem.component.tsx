2d6c6753be610f4258eb8be65de2b1ef251c7671
Thaneshwor Joshi <thaneshwor10@gmail.com>
2023-09-12 22:18:54 +0545

    feat: Make Battle History Page Responsive

--- src/components/molecules/BattleHistoryItem/BattleHistoryItem.component.tsx
+++ src/components/molecules/BattleHistoryItem/BattleHistoryItem.component.tsx
@@ -18,7 +18,7 @@ export const BattleHistoryItem: FC<BattleHistoryItemProps> = ({
           <span className="battle-history-item__datetime-title">Date</span>
           <span className="battle-history-item__value">{battleDate}</span>
         </div>
-        <div className="battle-history-item__datetime">
+        <div className="battle-history-item__datetime battle-history-item__time">
           <span className="battle-history-item__datetime-title">Time</span>
           <span className="battle-history-item__value">{battleTime}</span>
         </div>
@@ -27,10 +27,16 @@ export const BattleHistoryItem: FC<BattleHistoryItemProps> = ({
         <img
           src={pokemons?.[0].imageUrl}
           alt={pokemons?.[0].imageUrl}
-          className="battle-history-item__image battle-history-item__image--left"
+          className={`battle-history-item__image battle-history-item__image--left ${
+            userGuess === pokemons?.[0]?.name ? 'user-guess__img' : ''
+          }`}
+          title={pokemons?.[0]?.name}
         />
+        {winner?.name === pokemons?.[0]?.name && (
+          <img src={'./assets/trophy.png'} className="winner-img" />
+        )}
         <p
-          className={`battle-history-item__value ${
+          className={`battle-history-item__value battle-history-item__name ${
             userGuess === pokemons?.[0]?.name && 'user-guess'
           }`}
         >
@@ -38,7 +44,7 @@ export const BattleHistoryItem: FC<BattleHistoryItemProps> = ({
         </p>
         <img src="./assets/vs.png" className="battle-history-item__vs" />
         <p
-          className={`battle-history-item__value ${
+          className={`battle-history-item__value battle-history-item__name ${
             userGuess === pokemons?.[1]?.name && 'user-guess'
           }`}
         >
@@ -47,11 +53,19 @@ export const BattleHistoryItem: FC<BattleHistoryItemProps> = ({
         <img
           src={pokemons?.[1].imageUrl}
           alt={pokemons?.[1].imageUrl}
-          className="battle-history-item__image battle-history-item__image--right"
+          className={`battle-history-item__image battle-history-item__image--right ${
+            userGuess === pokemons?.[1]?.name ? 'user-guess__img' : ''
+          }`}
+          title={pokemons?.[1]?.name}
         />
+        {winner?.name === pokemons?.[1]?.name && (
+          <img src={'./assets/trophy.png'} className="winner-img" />
+        )}
       </div>
       <div className="battle-history-item__column battle-history-item__column--winner">
-        <p className="battle-history-item__value">Winner</p>
+        <p className="battle-history-item__value battle-history-item__name">
+          Winner
+        </p>
         <p className="battle-history-item__winner-trophy">
           <img src={'./assets/trophy.png'} />
         </p>
@@ -59,6 +73,7 @@ export const BattleHistoryItem: FC<BattleHistoryItemProps> = ({
           src={winner?.imageUrl}
           alt={winner?.name}
           className="battle-history-item__image"
+          title={winner?.name}
         />
       </div>
     </div>
